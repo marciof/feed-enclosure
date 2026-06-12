9b7405a8a08dd41ca975e4b03da8d4d28cdcfa01
Márcio <dev@marciof.com>
2026-06-11 20:45:42 -0700

    Refactor helper script to list outdated PIP packages
    - Recursive
    - Accepts command-line paths
    - Prints matches per file with PIP info

--- scripts/list_pip_updates.sh
+++ scripts/list_pip_updates.sh
@@ -9,25 +9,67 @@
 # FIXME error handling
 # FIXME proper logging
 
-set -e -u
+set -o errexit -o nounset
 
 if command -v mktemp >/dev/null; then
-    mktemp_posix() {
-        mktemp
-    }
+    alias mktemp_posix=mktemp
 else
     mktemp_posix() {
-        echo 'mkstemp(template)' | m4 -D "template=${TMPDIR:-/tmp}/"
+        echo 'mkstemp(template)' | m4 --define= "template=${TMPDIR:-/tmp}/"
     }
 fi
+
+pip_list_outdated_pkgs() {
+    pip list --outdated | tail --lines +3
+}
+
+find_reqs_txt_files() {
+    find "$@" \
+        -type d -name '.*' \! -path "$1" -prune \
+        -o \
+        -type f -iname '*requirements.txt' -print
+}
+
+list_pkgs_from_reqs_txt() {
+    grep --no-filename --invert-match --extended-regexp '^$|^#' -- "$@" \
+        | cut --delimiter '=' --fields 1 \
+        | sort \
+        | uniq
+}
+
+grep_for_pkgs() {
+    xargs -I '{}' -- echo --regexp='{}' | xargs grep "$1" --fixed-strings
+}
+
+pretty_print_outdated_pkgs() {
+    echo "* $1 *"
+    printf "  %${#1}s  \n" | tr ' ' '-'
+
+    if ! list_pkgs_from_reqs_txt "$1" | grep_for_pkgs "$2"; then
+        echo '(N/A)'
+    fi
+}
+
+pretty_print_outdated_pkgs_or_dir() {
+    if [ -d "$1" ]; then
+        for reqs_txt_file in $(find_reqs_txt_files "$1"); do
+            echo
+            pretty_print_outdated_pkgs "$reqs_txt_file" "$2"
+        done
+    else
+        echo
+        pretty_print_outdated_pkgs "$1" "$2"
+    fi
+}
+
+outdated_pkgs_file="$(mktemp_posix)"
+echo "(Downloading list of outdated packages... $outdated_pkgs_file)"
+pip_list_outdated_pkgs > "$outdated_pkgs_file"
 
 if [ $# -eq 0 ]; then
-    REQS_TXT="$(dirname "$(readlink -e "$0")")/../requirements.txt"
-else
-    REQS_TXT="$1"
+    set -- .
 fi
 
-reqs_txt_patterns="$(mktemp_posix)"
-
-grep -vE '^$|^#' "$REQS_TXT" | cut -d'~' -f1 > "$reqs_txt_patterns"
-pip list --outdated | grep -Ff "$reqs_txt_patterns" -
+for arg; do
+    pretty_print_outdated_pkgs_or_dir "$arg" "$outdated_pkgs_file"
+done
