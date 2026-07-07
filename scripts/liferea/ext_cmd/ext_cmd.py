c82be43d99ef5c8de59f666a368a6ba03211383f
Márcio <dev@marciof.com>
2026-07-07 08:34:02 -0700

    Document non-obvious dependencies on `Liferea.Activatable` and `shell`

--- scripts/liferea/ext_cmd/ext_cmd.py
+++ scripts/liferea/ext_cmd/ext_cmd.py
@@ -1,34 +1,45 @@
 # -*- coding: UTF-8 -*-
 
-"""
-https://github.com/lwindolf/liferea/tree/main/plugins
-https://github.com/mozbugbox/liferea-plugin-studio
-"""
-
-# FIXME document (including dependencies and setup)
-#   https://github.com/lwindolf/liferea/blob/main/plugins/README.md#plugin-tutorial
-#   ~/.local/share/liferea/plugins/
+# FIXME document (including deps and setup ~/.local/share/liferea/plugins/)
 # FIXME tests (including typing, mypy, pycodestyle)
 # FIXME error handling
 # FIXME proper logging (including to syslog)
+# FIXME how to disable built-in Download Manager and have that setting persist?
+# FIXME confirm this is API compliant
+# FIXME use ShellActivatable and listen for new feed items?
+# TODO would be nice to optionally pass the feed article title
 
 # stdlib
 from pathlib import Path
 import subprocess
 import sys
 
-# FIXME requirements.txt PyGObject
+# internal
 from gi.repository import GObject, Liferea
 
 PLUGIN_PATH = Path(__file__)
 PLUGIN_NAME = PLUGIN_PATH.stem
 ENCLOSURE_URL_CMD = PLUGIN_PATH.parent / 'on_enclosure_url'
 
-# FIXME how to disable built-in Download Manager and have that setting persist?
-# FIXME confirm with author so this is API compliant
-# FIXME use ShellActivatable and listen for new feed items?
-class ExtCmdPlugin (GObject.Object, Liferea.Activatable, Liferea.DownloadActivatable):
+class ExtCmdPlugin (
+        GObject.Object,
+        Liferea.Activatable, # Required by `DownloadActivatable`.
+        Liferea.DownloadActivatable):
+
+    """
+    References:
+
+    - https://github.com/lwindolf/liferea/blob/main/plugins/README.md#plugin-tutorial
+    - https://github.com/lwindolf/liferea/blob/main/plugins/download-manager.py
+    - https://github.com/lwindolf/liferea/blob/main/src/plugins/download_activatable.c
+    - https://github.com/mozbugbox/liferea-plugin-studio
+    """
+
     __gtype_name__ = __qualname__
+
+    # Required by `DownloadActivatable`, even if not used:
+    #   gi/types.py: Warning: Object class [PLUGIN] doesn't implement property
+    #   'shell' from interface 'LifereaDownloadActivatable'
     shell = GObject.property(type=Liferea.Shell)
 
     def do_activate(self):
@@ -44,5 +55,4 @@ class ExtCmdPlugin (GObject.Object, Liferea.Activatable, Liferea.DownloadActivat
             % (PLUGIN_NAME, self.do_download.__name__, ENCLOSURE_URL_CMD, url),
             file=sys.stderr)
 
-        # TODO would be nice to optionally pass the feed article title
         subprocess.Popen([ENCLOSURE_URL_CMD, url])
