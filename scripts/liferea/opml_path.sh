972a555b65a4a6b2f714ded44766186f2c951856
Márcio <dev@marciof.com>
2026-07-07 10:59:31 -0700

    Link to source man page for reference

--- scripts/liferea/opml_path.sh
+++ scripts/liferea/opml_path.sh
@@ -5,7 +5,7 @@
 #
 # References:
 # - XDG Spec: https://specifications.freedesktop.org/basedir/latest/#variables
-# - Liferea source: https://raw.githubusercontent.com/lwindolf/liferea/refs/heads/main/src/node_source.c#:~:text=feedlist.opml
+# - Liferea man page: https://github.com/lwindolf/liferea/blob/main/data/man/liferea.1
 
 # TODO request Liferea cmdline flag to print path to OPML file
 
